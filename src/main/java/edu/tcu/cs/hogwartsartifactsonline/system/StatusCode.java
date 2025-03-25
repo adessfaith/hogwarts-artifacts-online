4a8d8321c4a9332b714e7b0a6501664b40b3fe66
adessfaith <adessasegura@gmail.com>
2025-03-24 20:59:51 -0500

    Artifact CRUD done! Close #1

---
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/system/StatusCode.java
@@ -0,0 +1,15 @@
+package edu.tcu.cs.hogwartsartifactsonline.system;
+
+public class StatusCode {
+    public static final int SUCCESS = 200;
+    public static final int ERROR = 500;
+    public static final int UNAUTHORIZED = 401;
+    public static final int NOT_FOUND = 404;
+    public static final int INTERNAL_SERVER_ERROR = 500;
+    public static final int INVALID_ARGUMENT = 400;
+
+
+
+
+
+}
