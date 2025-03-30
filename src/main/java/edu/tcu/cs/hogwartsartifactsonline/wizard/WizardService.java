37cc93ef4fc298e58cffa7503f4f38928f374b68
adessfaith <adessasegura@gmail.com>
2025-03-29 23:51:08 -0500

    Artifact assignment done Close Issue #3

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/wizard/WizardService.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/wizard/WizardService.java
@@ -1,5 +1,6 @@
 package edu.tcu.cs.hogwartsartifactsonline.wizard;
 
+import edu.tcu.cs.hogwartsartifactsonline.artifact.Artifact;
 import edu.tcu.cs.hogwartsartifactsonline.artifact.ArtifactRepository;
 import edu.tcu.cs.hogwartsartifactsonline.system.exception.ObjectNotFoundException;
 import jakarta.transaction.Transactional;
@@ -11,7 +12,11 @@ import java.util.List;
 @Transactional
 public class WizardService {
     private final WizardRepository wizardRepository;
+
+
     private final ArtifactRepository artifactRepository;
+
+
 
     public WizardService(WizardRepository wizardRepository, ArtifactRepository artifactRepository) {
         this.wizardRepository = wizardRepository;
@@ -43,6 +48,26 @@ public class WizardService {
         //before deletion, must unassign wizard's owned artifacts
             wizardToBeDeleted.removeAllArtifacts();
         this.wizardRepository.deleteById(wizardId);
+    }
+
+    public void assignArtifact(Integer wizardId, String artifactId){
+        //find artifact by id from the database
+
+        Artifact artifactToBeAssigned = this.artifactRepository.findById(artifactId).orElseThrow(()->new ObjectNotFoundException("artifact", artifactId));
+
+        //find wizard by id from the database
+         Wizard wizard = this.wizardRepository.findById(wizardId).orElseThrow(()->new ObjectNotFoundException("wizard", wizardId));
+
+
+        //artifact assignment
+
+        //We need to see if the artifact is already ownerd by some wizard
+        if(artifactToBeAssigned.getOwner() !=null){
+            artifactToBeAssigned.getOwner().removeArtifact(artifactToBeAssigned);
+        }
+        wizard.addArtifact(artifactToBeAssigned);
+
+
     }
 
 
